663553e64a441feb2b150f2acd39e00c336dc8d5
Anu <anucharley@gmail.com>
2021-06-11 20:51:31 -0400

    Feedback form

--- php/feedback_form.php
+++ php/feedback_form.php
@@ -13,8 +13,8 @@ function filter_email_header($form_field)
 $email_address  = filter_email_header($email_address);
 
 #Send email
-$headers = "From: $email_addressn";
-$sent = mail('you@domain.com', 'Feedback Form Submission', $feedback, $headers);
+$headers = "From: $email_address";
+$sent = mail('anucharley@gmail.com', 'Feedback Form Submission', $feedback, $headers);
 
 #Thank user or notify them of a problem
 if ($sent) {
